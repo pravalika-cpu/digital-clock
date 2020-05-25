fa2b8abc7befd31ef393e36790e11f50f5aa298c
pravalika-cpu <65751881+pravalika-cpu@users.noreply.github.com>
2020-05-25 21:57:28 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+var day=["sunday","monday","tuesday","wednesday","thursday","friday","saturda"]
+var myDate=new Date();
+var Day=myDate.getDay();
+document.write("today is:"+day[day])
+var hrs=myDate.getHours();
+var hr=(hrs)%12
+var min=myDate.getMinutes()
+if(hrs<12){
+document.write("good morning"+"time is"+hr+":"+min+"A.M")
+
+}
+else if(hrs>=12&&hrs<=17){
+    document.write("good afternoon"+"time is"+hr+":"+min+"P.M")
+}
+else{
+    document.write("good evenging"+"time is"+hr+"min"+"P.M")
+}
